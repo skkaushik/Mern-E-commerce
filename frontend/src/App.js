28efcc0c2225cd056caf3c942f7e997f32155dc5
sachin <sksachinkaushik5362@gmail.com>
2023-07-05 21:59:14 +0530

    commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 
 import './App.css';
-import Footer from './Footer';
-import Navbar from './Navbar';
+import Footer from './components/Footer';
+import Navbar from './components/Navbar'
 import { BrowserRouter ,Route,Routes} from 'react-router-dom';
 
 function App() {
