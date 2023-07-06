ea697b20f61bba131144aa5c64bd0db364a741af
sachin <sksachinkaushik5362@gmail.com>
2023-07-06 10:45:51 +0530

    commit

--- frontend/src/components/SignUp.js
+++ frontend/src/components/SignUp.js
@@ -7,31 +7,21 @@ function SignUp() {
   const [password,setPassword]=useState("");
   const [email,setEmail]=useState("");
   const [age,setAge]=useState("")
-  // const[user,setUser]=useState({
-  //   name:"",
-  //   email:"",
-  //   password:"",
-  //   age:
-  // })
+
   const navigate=useNavigate();
 
   const collectData=async ()=>{
 
     
     console.log(name,email,password,age)
-    // let result = await fetch('http://localhost:5000/register',{
-    //  methdo:'POST',
-    //  body:JSON.stringify({name,email,password,age}),
-    //  headers:{
-    //   'Content-Type':'application/json'
-    //  },
-    // });
-    // result=await result.json();
-    // console.log(result);
+   
     axios.post("http://localhost:5000/register",{name,email,password,age})
     .then((res)=>{
       console.log(res.data);
+      if(res.data){
+        alert("Yor have register successfully!")
       navigate("/")
+      }
     
     }).catch(err=>console.log(err))
   }
