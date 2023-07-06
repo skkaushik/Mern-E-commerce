7a9872f5db358e80aadb9f996fc5ddd029b00cf5
sachin <sksachinkaushik5362@gmail.com>
2023-07-06 12:14:20 +0530

    commit

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,25 @@ app.use(express.json());
 app.post('/register',async(req,res)=>{
     let user=new User(req.body);
     let result = await user.save();
+    result =result.toObject();
+    delete result.password;
     res.send(result);
+})
+
+app.post('/login',async(req,res)=>{
+    if(req.body.email && req.body.password){
+
+    let user=await User.findOne(req.body).select("-password");
+    if(user){
+        res.send(user)
+
+    }else{
+        res.send({result:'No user Found'})
+    }
+}else{
+    res.send({result: "No User Found"})
+}
+
 })
 
 
