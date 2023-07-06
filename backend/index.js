fbcef60f948d47fdfadffeeed3e8d80b91629bac
sachin <sksachinkaushik5362@gmail.com>
2023-07-06 10:38:23 +0530

    commit

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,21 @@
 const express=require('express');
+const cors=require('cors');
+require('./db/config');
+const User=require("./db/User")
 
 const app=express();
+app.use(cors());
 
 
-app.get('/',(req,res)=>{
-    res.send("This is Working........")
-});
+app.use(express.json());
+
+app.post('/register',async(req,res)=>{
+    let user=new User(req.body);
+    let result = await user.save();
+    res.send(result);
+})
+
+
 
 app.listen(5000,()=>{
     console.log("Server is running")
--- frontend/src/components/SignUp.js
+++ frontend/src/components/SignUp.js
@@ -1,13 +1,39 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
+import axios from "axios";
+import {useNavigate} from'react-router-dom';
 
 function SignUp() {
   const [name,setName]=useState('');
   const [password,setPassword]=useState("");
   const [email,setEmail]=useState("");
   const [age,setAge]=useState("")
+  // const[user,setUser]=useState({
+  //   name:"",
+  //   email:"",
+  //   password:"",
+  //   age:
+  // })
+  const navigate=useNavigate();
 
-  const collectData=()=>{
+  const collectData=async ()=>{
+
+    
     console.log(name,email,password,age)
+    // let result = await fetch('http://localhost:5000/register',{
+    //  methdo:'POST',
+    //  body:JSON.stringify({name,email,password,age}),
+    //  headers:{
+    //   'Content-Type':'application/json'
+    //  },
+    // });
+    // result=await result.json();
+    // console.log(result);
+    axios.post("http://localhost:5000/register",{name,email,password,age})
+    .then((res)=>{
+      console.log(res.data);
+      navigate("/")
+    
+    }).catch(err=>console.log(err))
   }
   return (
     <div>
